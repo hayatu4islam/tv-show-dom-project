e4db188253481cc3ebc7f9027396ef9e63befe5f
hayatu4islam <hayatu4islam@yahoo.com>
2022-10-13 23:53:02 +0100

    Solved level 200

--- script.js
+++ script.js
@@ -1,26 +1,67 @@
 //You can edit ALL of the code here
 function setup() {
-  const allEpisodes = getAllEpisodes();
+  var allEpisodes = getAllEpisodes();
+  const oneEpi = getOneEpisode();
+  // console.log(oneEpi);
   makePageForEpisodes(allEpisodes);
 }
 
-const searchField = document.getElementById("search-field");
-searchField.addEventListener("key", (e) => {
-  console.log(e.target.value);
-  const episodes = getAllEpisodes();
-  const episodesFound = episodes.filter(episode => {
-    episode.name.includes(e.target.value) || episode.summary.includes(e.target.value);
-  })
-  makePageForEpisodes(episodesFound);
-})
+var containerHead = document.createElement("div");
+containerHead.setAttribute("class", "container-head");
+document.body.prepend(containerHead);
 
+const searchField = document.createElement("input");
+searchField.id = "input";
+searchField.setAttribute("autocomplete", "off");
+searchField.setAttribute("placeholder", "Search for episodes");
+containerHead.appendChild(searchField);
+
+
+// ----------UPDATE EPISODE LISTS-----------------------
+// const searchField = document.getElementById("input");
+const rootElement = document.getElementById("root");
+const matchEpisodeDisplay = document.createElement("label");
+// matchEpisodeDisplay.innerHTML = "";
+matchEpisodeDisplay.innerHTML = "73 / 73 Episodes are found";
+matchEpisodeDisplay.setAttribute("class", "displayLabel");
+containerHead.appendChild(matchEpisodeDisplay);
+
+searchField.addEventListener("keyup", modifyEpisodes);
+let episodesFound = "";
+
+let allEpisodes = getAllEpisodes();
+function modifyEpisodes(e){
+  let currentValue = e.target.value.toLowerCase();
+  rootElement.innerHTML = "";
+  episodesFound = allEpisodes.filter(
+    (ep) =>
+    ep.name.toLowerCase().includes(currentValue) ||
+    ep.summary.toLowerCase().includes(currentValue));
+    console.log(episodesFound);
+    makePageForEpisodes(episodesFound);
+    matchEpisodeDisplay.innerHTML = ` ${episodesFound.length} / ${allEpisodes.length} Episodes are found`;
+  }
+
+  // searchField.addEventListener("keyup", (e) => {
+  //   // console.log(e.target.value);
+  //   const episodes = getAllEpisodes();
+  //   let episodesFound = "";
+  //   episodesFound = episodes.filter((epi) => {
+  //     epi.name.toLowerCase().includes(e.target.value.toLowerCase()) || epi.summary.toLowerCase().includes(e.target.value.toLowerCase());
+  //   });
+  //   makePageForEpisodes(episodesFound);
+  // });
+// }
+
+
+// ---------------MAKE PAGE FOR EPISODES FUNCTION---------------------
 function makePageForEpisodes(episodeList) {
-  console.log(episodeList);
+  // console.log(episodeList);
   const rootElem = document.getElementById("root");
   rootElem.className = "root";
   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 
-  episodeList.forEach(episode => {
+  episodeList.forEach((episode) => {
     const episodesContainer = document.createElement("div");
     episodesContainer.setAttribute("class", "episode-container");
     rootElem.appendChild(episodesContainer);
